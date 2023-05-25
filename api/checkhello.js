78b0cd506a08a7722b3b4cb7667650effe70094f
Tawatchai Phetdumrongsakul <tawatchai.phe@ttbspark.com>
2023-05-25 12:09:07 +0700

    update print env

--- api/checkhello.js
+++ api/checkhello.js
@@ -3,6 +3,8 @@ module.exports = async (req, res) => {
     console.log('my function is running')
     const { token = 'none' } = req.query
     console.log(token)
+    console.log('env PACKAGE_NAME is')
+    console.log(process.env.PACKAGE_NAME)
 
     res.status(200).send('hello world by tawatchai')
 }
